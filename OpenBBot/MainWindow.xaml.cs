d351c630fb5ae066be85379d3cf84dad83cba55a
Bruno Furtado <brunofurmon@poli.ufrj.br>
2017-06-04 21:46:52 -0300

    Integer validation on Interval field

--- OpenBBot/MainWindow.xaml.cs
+++ OpenBBot/MainWindow.xaml.cs
@@ -1,8 +1,8 @@
-﻿using OpenBBot.Services;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OpenBBot.Services;
+using OpenBBot.Validators;
 
 
 namespace OpenBBot
@@ -22,12 +24,24 @@ namespace OpenBBot
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static ClickingThreadService ClickingThread = new ClickingThreadService(20);
+        private readonly ClickingThreadService ClickingThread;
 
         public MainWindow()
         {
+            this.ClickingThread = new ClickingThreadService(20);
             GlobalListenerService.InstallHook(ClickingThread.Switch);
             InitializeComponent();
+        }
+
+        // Validates Numbers Only
+        private void IntervalInput_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !InputValidators.ValidateAgainstNumber(e.Text);
+        }
+
+        private void IntervalInput_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = e.Key == Key.Space;
         }
     }
 }
---
+++ OpenBBot/Validators/InputValidators.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace OpenBBot.Validators
+{
+    public static class InputValidators
+    {
+        public static bool ValidateAgainstNumber(string text)
+        {
+            if (string.IsNullOrEmpty(text) || !char.IsDigit(text, text.Length - 1))
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool IsInsideLimits(Int32 number, int min = Int32.MinValue, int max = Int32.MaxValue)
+        {
+            bool isInsideLimits = number <= min && number <= max;
+            return isInsideLimits;
+        }
+    }
+}
